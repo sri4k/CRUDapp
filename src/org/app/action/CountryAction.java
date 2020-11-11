d690c5a686fbe4988f4452ec2c1a4965853489aa
sri4k <sricharanharithas@gmail.com>
2020-11-11 19:14:23 +0530

    Includes: ivy.xml, build.xml, appilcationContext.xml, hibernate, spring, struts changes for curd app

--- src/org/app/action/CountryAction.java
+++ src/org/app/action/CountryAction.java
@@ -1,19 +1,33 @@
 package org.app.action;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.app.dao.CountryDao;
 import org.app.model.Country;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CountryAction {
 	
-	ArrayList<Country> list = new ArrayList<Country>();
+	List<Country> list = new ArrayList<Country>();
 	String countryCode;
 	String countryName;
 	String capitalCity;
 	String currencyCode;
+	@Autowired
+	CountryDao countryDao;
 	
-	public ArrayList<Country> getList() {
+	public CountryDao getCountryDao() {
+		return countryDao;
+	}
+
+	public void setCountryDao(CountryDao countryDao) {
+		this.countryDao = countryDao;
+	}
+
+	public List<Country> getList() {
 		return list;
 	}
 
@@ -56,7 +70,7 @@ public class CountryAction {
 	public String getCountries() {
      
 		try {
-			list = CountryDao.getCountries();
+			list = countryDao.getCountries();
 			return "success";
 		}catch(Exception e){
 			e.printStackTrace();
@@ -68,12 +82,40 @@ public class CountryAction {
 		Country country = new Country(countryCode, countryName, capitalCity, currencyCode);
 
 		try {
-			CountryDao.save(country);
-			list = CountryDao.getCountries();
+			countryDao.saveCountry(country);
+			list = countryDao.getCountries();
 			return "success";
 		}catch(Exception e){
 			e.printStackTrace();
 			return "error";}
 		
 	}
+	
+//	HibernateTemplate template;  
+//	public void setTemplate(HibernateTemplate template) {  
+//	    this.template = template;  
+//	}  
+//	//method to save employee  
+//	public void saveEmployee(Employee e){  
+//	    template.save(e);  
+//	}  
+//	//method to update employee  
+//	public void updateEmployee(Employee e){  
+//	    template.update(e);  
+//	}  
+//	//method to delete employee  
+//	public void deleteEmployee(Employee e){  
+//	    template.delete(e);  
+//	}  
+//	//method to return one employee of given id  
+//	public Employee getById(int id){  
+//	    Employee e=(Employee)template.get(Employee.class,id);  
+//	    return e;  
+//	}  
+//	//method to return all employees  
+//	public List<Employee> getEmployees(){  
+//	    List<Employee> list=new ArrayList<Employee>();  
+//	    list=template.loadAll(Employee.class);  
+//	    return list;  
+//	}  
 }
